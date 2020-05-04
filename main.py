447c38fbb8688bad4610a6efb21868e58c00a039
CodeJou <joumaa_Family@icloud.com>
2020-05-04 00:45:35 -0400

    Fixed weird JSON parsing bug, got api query working.

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import json
 def config():
     try:
         with open("./config.json", "r") as f:
-            config = json.loads(f.read())
+            global city, country
+            config = json.loads(str(f.read()))
             city = config["city"]
             country = config["country"]
-            global city, country
             f.close()
     #No config file, probably first use of tool
     except IOError:
@@ -16,8 +16,7 @@ def config():
             print("[autoIftar] No configuration file found. This seems to be your first time using this tool.")
             city = raw_input("[autoIftar] Please enter your city (Spaces and Capitalizations Included): ")
             country = raw_input("[autoIftar] Please enter your country (Spaces and Capitalizations Included): ")
-            global city, country
-            json.dump('{"city":"%s", "country":"%s"}' % (city, country), f)
+            f.write('{"city":"%s","country":"%s"}' % (city, country) )
             f.close()
     try:
         from playsound import playsound
@@ -32,6 +31,8 @@ def config():
                 break
             else:
                 print("[autoIftar] Wrong input")
+
+def queryAPIMaghrib():
     try:
         import requests
     except ImportError:
@@ -45,14 +46,15 @@ def config():
                 break
             else:
                 print("[autoIftar] Wrong input")
-
-def queryAPIMaghrib():
     response = requests.get("http://api.aladhan.com/v1/timingsByCity?city={}&country={}&method=8".format(city, country))
-    if response.status_code !== 200:
+    json_response = response.json()
+    if response.status_code != 200 or json_response["status"] != "OK":
         print("[autoIftar] Something went wrong. Check your internet connection and your spelling.")
         exit(1)
-    json_response = response.json()
-    
+    maghrib_time = int(str(json_response["data"]["timings"]["Maghrib"]).replace(":", ""))
+
+def playAthan():
+    print("Adkl")
 if __name__ == '__main__':
     config()
     queryAPIMaghrib()
